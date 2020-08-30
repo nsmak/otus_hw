0ef70e6749b6df7f1c439e3d7c026ae486ce019e
Alexandr Chirkov <a.chirkov@globus-ltd.com>
2020-08-30 16:03:37 +0300

    Реализован вывод локального времени и точного времени с использованием библиотеки NPT.

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
+const host = "0.beevik-ntp.pool.ntp.org" // этот параметр для удобства можно вынести в качестве флага
+
 func main() {
-	// Place your code here
+	ntpTime, err := ntp.Time(host)
+	if err != nil {
+		log.Fatalf("can't get time from remote host %s: %v", host, err)
+	}
+	fmt.Printf("current time: %v\n", time.Now().Round(0))
+	fmt.Printf("exact time: %v\n", ntpTime.Round(0))
 }
