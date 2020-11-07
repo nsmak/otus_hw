c284f67531b3c2f9aeb6a31189369f180f9103a1
Alexandr Chirkov <a.chirkov@globus-ltd.com>
2020-11-08 01:40:07 +0300

    Реализована утилиту envdir

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,10 +1,62 @@
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path"
+	"strings"
+)
+
 type Environment map[string]string
 
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	infos, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	env := make(Environment)
+
+	for _, info := range infos {
+		if info.IsDir() {
+			return nil, fmt.Errorf("it's not a file: %v", info.Name())
+		}
+		if strings.Contains(info.Name(), "=") {
+			return nil, fmt.Errorf("invalid variable name: %v", info.Name())
+		}
+
+		name := info.Name()
+
+		file, err := os.Open(path.Join(dir, name))
+		if err != nil {
+			return nil, err
+		}
+
+		r := bufio.NewReader(file)
+		l, _, err := r.ReadLine()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				env[name] = ""
+				continue
+			}
+			return nil, err
+		}
+
+		l = bytes.ReplaceAll(l, []byte("\x00"), []byte("\n"))
+		l = bytes.TrimRight(l, "\t \n")
+
+		var builder strings.Builder
+		builder.Write(l)
+
+		env[name] = builder.String()
+	}
+
+	return env, nil
 }
--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,61 @@
 package main
 
-// RunCmd runs a command + arguments (cmd) with environment variables from env
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+// RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here
-	return
+	if len(cmd) == 0 {
+		fmt.Println("need input command")
+		return 1
+	}
+
+	var args []string
+	if len(cmd) > 1 {
+		args = append(args, cmd[1:]...)
+	}
+
+	err := prepareOSEnv(env)
+	if err != nil {
+		fmt.Printf("can't prepare os environment: %v", err)
+		return 1
+	}
+
+	command := exec.Command(cmd[0], args...) //nolint:gosec
+	command.Stdin = os.Stdin
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+	command.Env = os.Environ()
+
+	err = command.Run()
+	if err != nil {
+		var exitErr *exec.ExitError
+		if errors.As(err, &exitErr) {
+			return exitErr.ExitCode()
+		}
+		fmt.Printf("unknown command error: %v\n", err)
+		return 1
+	}
+	return 0
+}
+
+func prepareOSEnv(env Environment) error {
+	for key, value := range env {
+		if len(value) == 0 {
+			err := os.Unsetenv(key)
+			if err != nil {
+				return err
+			}
+			continue
+		}
+		err := os.Setenv(key, value)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
