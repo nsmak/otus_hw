b4a67026598b33bcc1710ee14213696d41076c9a
Alexandr Chirkov <a.chirkov@globus-ltd.com>
2021-01-10 16:00:23 +0300

    добавлено удаление волюмов

--- hw12_13_14_15_calendar/deployments/deploy.sh
+++ hw12_13_14_15_calendar/deployments/deploy.sh
@@ -9,6 +9,7 @@ up)
 down)
   docker-compose -f deployments/docker-compose.yaml down
   docker network rm cal_network
+  docker volume rm deployments_dbdata
   ;;
 
 tests)
@@ -18,7 +19,8 @@ tests)
   	docker-compose -f deployments/docker-compose-tests.yaml up --build
   	rc=$?
   	docker-compose -f deployments/docker-compose.yaml -f deployments/docker-compose-tests.yaml down
-  	 docker network rm cal_network
+  	docker network rm cal_network
+  	docker volume rm deployments_dbdata
   	 exit $rc
   	 ;;
 *)
